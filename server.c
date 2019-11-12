3afb12442bbe5e9b05e2081bfa8130fabf2dd916
Numfor Tiapo <numsmt2@gmail.com>
2019-11-12 12:00:17 -0500

    Handle senser in threads

--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h> 
 
 #define MAX_BUFFER 512
 #define MAX_STATIONS 256
@@ -12,7 +13,21 @@
 #define MAX_LINKS 512
 #define MAX_STATION_LENGTH 512
 
+//keeps track of the index of the client socket
+typedef struct {
+  int i;
+} Index;
+
+
+void* listenForConnections(void* p);
+void* handleSensor(void* p);
+
 fd_set readfds; //keeps track of the sockets that 'select will listen for'
+unsigned short controlPort;
+int client_sockets[ MAX_STATIONS ]; /* client socket fd list */
+int client_socket_index = 0;  /* next free spot */
+
+pthread_mutex_t lock;
 
 int main(int argc, char* argv[]) {
 	// Check for correct number of command line arguments
@@ -29,7 +44,7 @@ int main(int argc, char* argv[]) {
 	int k;
 
 	// Control port argument and base station file argument
-	unsigned short controlPort = atoi(argv[1]);
+	controlPort = atoi(argv[1]);
 	const char* baseStationFile = argv[2];
 
 	//PARSING BASE STATION FILE-------------------------------------------
@@ -142,6 +157,45 @@ int main(int argc, char* argv[]) {
 
 	fclose(file);
 
+	//initialize the mutex
+	if (pthread_mutex_init(&lock, NULL) != 0) { 
+	  perror("\n mutex init has failed\n"); 
+	  return 1; 
+	} 
+
+    pthread_t tid;
+    pthread_create(&tid, NULL, listenForConnections, NULL);
+
+    char stdin_buffer[MAX_BUFFER];
+    int num_bytes = 0;
+
+	//Main thread blocking on stdin
+	while(fgets(stdin_buffer, MAX_BUFFER, stdin)){
+		stdin_buffer[strcspn(stdin_buffer, "\n")] = 0; //remove newline from stdin_buffer
+		num_bytes = strlen(stdin_buffer);
+		printf("Read %s from stdin, %d bytes\n", stdin_buffer, num_bytes);
+		memset(stdin_buffer, 0, MAX_BUFFER);  
+	}
+
+
+	// Freeing the allocated memory
+	for(i=0; i<MAX_STATIONS; i++) {
+		free(baseStations[i]);
+		free(stationCoordinates[i]);
+		for(j=0; j<MAX_LINKS; j++) {
+			free(stationLinks[i][j]);
+		}
+		free(stationLinks[i]);
+	}
+	free(baseStations);
+	free(stationCoordinates);
+	free(stationNumLinks);
+	free(stationLinks);
+
+	return EXIT_SUCCESS;
+}
+
+void* listenForConnections(void* p){
 	//SETTTING UP TCP SERVER----------------------------------------------
 
 	//Creating listener socket
@@ -182,31 +236,74 @@ int main(int argc, char* argv[]) {
 		FD_ZERO( &readfds ); //initializes the file descriptor set
 		FD_SET( sd, &readfds ); //select() will check tcp socket for activity
 
-		//If there is activity on the socket
+    	select( FD_SETSIZE, &readfds, NULL, NULL, NULL ); //blocking
+
+		//If there is a new connection being made
 		if ( FD_ISSET( sd, &readfds ) ) {
       		int newsock = accept( sd, (struct sockaddr *)&client, (socklen_t *)&fromlen );
       		printf("MAIN: Rcvd incoming TCP connection from: %s\n", inet_ntoa( (struct in_addr)client.sin_addr ));
-
+      		client_sockets[ client_socket_index++ ] = newsock;
+      		Index* ind = malloc(sizeof(Index));
+      		ind->i = client_socket_index-1;
+      		pthread_t tid;
+      		pthread_create(&tid, NULL, handleSensor, (void*) ind);
 		}
 	}
+}
+
+void* handleSensor(void* p){
+	//Get the fd this child socket should be listening on 
+	Index* ind = (Index*) p;
+	int index = ind->i;
+	int fd = client_sockets[ index ];
+  	char buffer[ MAX_BUFFER ];
 
 
+	while(1){
+		printf("CHILD %lu: Blocking on recv\n", pthread_self());
+		int num_bytes = recv( fd, buffer, MAX_BUFFER - 1, 0 );
+		printf("CHILD %lu: recieved %d bytes, %s\n", pthread_self(), num_bytes, buffer);
+		if(num_bytes < 0){
 
+			perror("recv()");
 
+		}else if ( num_bytes == 0 ){
 
-	// Freeing the allocated memory
-	for(i=0; i<MAX_STATIONS; i++) {
-		free(baseStations[i]);
-		free(stationCoordinates[i]);
-		for(j=0; j<MAX_LINKS; j++) {
-			free(stationLinks[i][j]);
+			printf("CHILD %lu: Client disconnected\n", pthread_self());
+			pthread_mutex_lock(&lock);
+			int k;
+			close( fd );
+
+			// if(is_logged_in){
+			//   deleteUser(current_user.userid);
+			//   is_logged_in = false;  
+			// }   
+
+			/* remove fd from client_sockets[] array: */
+			for ( k = 0 ; k < client_socket_index ; k++ )
+			{
+			  if ( fd == client_sockets[ k ] )
+			  {
+			    /* found it -- copy remaining elements over fd */
+			    int m;
+			    for ( m = k ; m < client_socket_index - 1 ; m++ )
+			    {
+			      client_sockets[ m ] = client_sockets[ m + 1 ];
+			    }
+			    client_socket_index--;
+			    break;  /* all done */
+			  }
+			}
+			pthread_mutex_unlock(&lock);
+			// printf("NUMBER OF CONNECTIONS: %d\n", client_socket_index);
+			break;
+
+		} else {
+
+			int n = send(fd, buffer, num_bytes, 0);
+			if(n != num_bytes)
+            	perror( "send() failed" );
+			memset(buffer, 0, MAX_BUFFER);  
 		}
-		free(stationLinks[i]);
 	}
-	free(baseStations);
-	free(stationCoordinates);
-	free(stationNumLinks);
-	free(stationLinks);
-
-	return EXIT_SUCCESS;
 }
